ea637858f66f5b73cfeebf932eb76b54f8451ed0
sebiva <ivarsson.sebastian@gmail.com>
2013-09-19 16:09:29 +0200

    Fixes and Docs

--- src/com/example/fortytwo/MainActivity.java
+++ src/com/example/fortytwo/MainActivity.java
@@ -13,21 +13,25 @@ import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.Toast;
-
+/**
+ * The main activity of the application, contains a grid with 
+ * piles represented as buttons.  
+ *
+ */
 public class MainActivity extends Activity implements OnClickListener {
 
 	private static int NUM_ROWS = 3;
 	private static int NUM_COLUMNS = 7;
 	private TableLayout tl;
+	//Contains all buttons of the table
 	private ArrayList<Button> buttons = new ArrayList<Button>();
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.activity_main);
-
-		setupButtons();
+		// Create Buttons in the tableview 
+		setupButtons(NUM_ROWS,NUM_COLUMNS);
 	}
 
 
@@ -38,24 +42,30 @@ public class MainActivity extends Activity implements OnClickListener {
 		return true;
 	}
 
-
-	public void setupButtons() {
+	/**
+	 * Creates the buttons in a grid on the "table". The number of rows and columns
+	 * are specified by NUM_ROWS and NUM_COLUMNS. 
+	 * 
+	 */
+	public void setupButtons(int num_rows, int num_columns) {
 		tl = (TableLayout) findViewById(R.id.tableTable);
-		for (int i=0 ; i<NUM_ROWS ; i++) {
+		//Create a number of rows in the table
+		for (int i=0 ; i<num_rows ; i++) {
 			TableRow tr = new TableRow(this);
 			tr.setTag("row" + i);
-			
+			//Create the layout parameters for the table row, all rows should be the same size
 			LayoutParams tp = new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.MATCH_PARENT, 1.0f);
-			for (int j=0; j<NUM_COLUMNS; j++) {
+			for (int j=0; j<num_columns; j++) {
 				Button b = new Button(this);
+				//Create the layout parameters for the button, all buttons should be the same size
 				LayoutParams bp = new TableRow.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.MATCH_PARENT, 1.0f);
 
-				b.setTag("button" + (NUM_COLUMNS*i + j));
-				b.setText("Pile " + (NUM_COLUMNS*i + j));
+				b.setTag("button" + (num_columns*i + j));
+				b.setText("Pile " + (num_columns*i + j));
 				b.setPadding(5, 5, 5, 5);
 				tr.addView(b);
 				buttons.add(b);
-				b.callOnClick();
+				//Set this interface as the listener to the button
 				b.setOnClickListener(this);
 				b.setTextColor(0xffffffff);
 				b.setBackgroundResource(R.drawable.ic_launcher);
