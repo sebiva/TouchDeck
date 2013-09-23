03484c3faca3d4230492da00da781d70397bfcbb
sebiva <ivarsson.sebastian@gmail.com>
2013-09-23 13:37:36 +0200

    Improved startscreen, added functionality to button, now launching local game

--- src/se/chalmers/touchdeck/gui/StartScreen.java
+++ src/se/chalmers/touchdeck/gui/StartScreen.java
@@ -3,8 +3,10 @@ package se.chalmers.touchdeck.gui;
 import com.example.fortytwo.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class StartScreen extends Activity {
 	
@@ -14,11 +16,15 @@ public class StartScreen extends Activity {
 		setContentView(R.layout.start_screen);
 	}
 
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void startLocal(View v) {
+		Intent localGame = new Intent(this, TableView.class);
+		startActivity(localGame);
 	}
 }
