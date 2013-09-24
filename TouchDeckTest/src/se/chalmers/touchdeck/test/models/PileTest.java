89191a89808a7c8a518f2351a0f48a97a33640cc
Karl Engstrom <karl.engstrom@hotmail.com>
2013-09-24 16:44:10 +0200

    Added TouchDeckTest project

---
+++ TouchDeckTest/src/se/chalmers/touchdeck/test/models/PileTest.java
@@ -0,0 +1,14 @@
+package se.chalmers.touchdeck.test.models;
+
+import junit.framework.TestCase;
+
+public class PileTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	// Identifiering av pile:
+	// b.setId(NUM_COLUMNS * i + j);
+	// b.setTag("Pile " + (NUM_COLUMNS * i + j));
+}
