662b50627643af1a1b4b676458836dbffa7b81a4
Karl Engstrom <karl.engstrom@hotmail.com>
2013-09-27 08:23:09 +0200

    Added test code

---
+++ TouchDeckTest/src/se/chalmers/touchdeck/test/gui/PileViewTest.java
@@ -0,0 +1,44 @@
+
+package se.chalmers.touchdeck.test.gui;
+
+import se.chalmers.touchdeck.R;
+import se.chalmers.touchdeck.gui.PileView;
+import se.chalmers.touchdeck.gui.TableView;
+import android.R.string;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+
+import com.jayway.android.robotium.solo.Solo;
+
+public class PileViewTest extends ActivityInstrumentationTestCase2<PileView> {
+
+	private PileView tableView;
+
+	public PileViewTest(){
+		super(PileView.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		setActivityInitialTouchMode(false); 	// Turn off touch mode in the emulator.
+		tableView = getActivity();				// Get the Activity under test, starting it if necessary.
+	}
+	
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		setActivityInitialTouchMode(true); 		// Turn on touch mode in the emulator.
+	}
+	
+//	public void testButtonPressed(){
+//		Solo solo = new Solo(getInstrumentation(), tableView);
+//		solo.clickOnView(tableView.findViewById(tableView.getResources().getInteger(R.integer.initial_pile_id)+1));
+//		solo.clickOnButton("OK");		
+//	}
+	
+	public void testInspectPile(){
+		Solo solo = new Solo(getInstrumentation(), tableView);
+		solo.clickOnView(tableView.findViewById(tableView.getResources().getInteger(R.integer.initial_pile_id)));
+		solo.clickOnButton(0);
+		solo.clickOnView(solo.getView(R.id.menu_item_flip));
+	}
+}
--- TouchDeckTest/src/se/chalmers/touchdeck/test/gui/TableViewTest.java
+++ TouchDeckTest/src/se/chalmers/touchdeck/test/gui/TableViewTest.java
@@ -1,33 +1,43 @@
 package se.chalmers.touchdeck.test.gui;
 
 import se.chalmers.touchdeck.R;
+import se.chalmers.touchdeck.gui.PileView;
 import se.chalmers.touchdeck.gui.TableView;
+import android.R.string;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
 
+import com.jayway.android.robotium.solo.Solo;
 
 public class TableViewTest extends ActivityInstrumentationTestCase2<TableView> {
 
-	private TableView view;
-
+//	private TableView tableView;
+//
 	public TableViewTest(){
 		super(TableView.class);
 	}
-
-	protected void setUp() throws Exception {
-		super.setUp();
-		setActivityInitialTouchMode(false); // Turn off touch mode in the emulator.
-		view = getActivity();				// Get the Activity under test, starting it if necessary.
-	}
+//
+//	protected void setUp() throws Exception {
+//		super.setUp();
+//		setActivityInitialTouchMode(false); 	// Turn off touch mode in the emulator.
+//		tableView = getActivity();				// Get the Activity under test, starting it if necessary.
+//	}
+//	
+//	protected void tearDown() throws Exception {
+//		super.tearDown();
+//		setActivityInitialTouchMode(true); 		// Turn on touch mode in the emulator.
+//	}
+//	
+//	public void testButtonPressed(){
+//		Solo solo = new Solo(getInstrumentation(), tableView);
+//		solo.clickOnView(tableView.findViewById(tableView.getResources().getInteger(R.integer.initial_pile_id)+1));
+//		solo.clickOnButton("OK");		
+//	}
 	
-	protected void tearDown() throws Exception {
-		super.tearDown();
-		setActivityInitialTouchMode(true); // Turn on touch mode in the emulator.
-	}
-	
-	public void testButtonPressed(){
-		TouchUtils.clickView(this, view.findViewById(view.getResources().getInteger(R.integer.initial_pile_id)));
-		view = getActivity();
-		TouchUtils.clickView(this, view.findViewById(0));
-	}
+//	public void testInspectPile(){
+//		Solo solo = new Solo(getInstrumentation(), tableView);
+//		solo.clickOnView(tableView.findViewById(tableView.getResources().getInteger(R.integer.initial_pile_id)));
+//		solo.clickOnButton(0);
+//		solo.clickOnView(solo.getView(R.id.menu_item_move));
+//	}
 }
